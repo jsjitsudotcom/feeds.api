537c093292e63f9f4b6b2139e5b01bc11eb33776
MarquesDev <marques.developpement@gmail.com>
2019-01-13 12:21:03 +0100

    chore: adding sequelize and docker

---
+++ models/user.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const user = sequelize.define(
+    "user",
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING
+    },
+    {
+      underscored: true,
+      freezeTableName: true,
+      tableName: "user",
+      paranoid: true
+    }
+  );
+
+  user.associate = function(models) {
+    // associations can be defined here
+  };
+  return user;
+};
